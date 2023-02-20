8682ad3acab1af4fde98f56575d8a8bf9400806f
groloch <bjuran2001@gmail.com>
2023-02-20 17:57:48 +0100

    lidar physical function almost done

--- env.py
+++ env.py
@@ -1,35 +1,51 @@
 import pygame
 from pygame.locals import *
-from objects import Wall
+from objects import Wall, Lidar
+
+
+pygame.init()
+vec = pygame.math.Vector2
+
+WIDTH = 1280
+HEIGHT = 720
+FPS = 60
+
+fps = pygame.time.Clock()
+displaysurface = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Lidar Test")
+
+w1 = Wall(100, 100, 100, 100)
+w2 = Wall(800, 100, 100, 500)
+
+all_sprites = pygame.sprite.Group()
+all_sprites.add(w1)
+all_sprites.add(w2)
+
+walls = [w1, w2]
+lidar = Lidar(displaysurface, 200, 200, 0.03, walls)
 
 def display():
-    pygame.init()
-    vec = pygame.math.Vector2
 
-    HEIGHT = 1280
-    WIDTH = 720
-    ACC = 0.5
-    FRIC = -0.12
-    FPS = 60
-
-    fps = pygame.time.Clock()
-    displaysurface = pygame.display.set_mode((WIDTH, HEIGHT))
-    pygame.display.set_caption("Lidar Test")
-
-    w = Wall(10, 10, 100, 100)
-    all_sprites = pygame.sprite.Group()
-    all_sprites.add(w)
     
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
-                sys.exit()
         
-        displaysurface.fill((0,0,0))
+        displaysurface.fill((255,255,255))
     
         for entity in all_sprites:
             displaysurface.blit(entity.surf, entity.rect)
-    
+
+        lidar.update()
+
         pygame.display.update()
         fps.tick(FPS)
+
+def isValid(x, y):
+    if x < 0 or y < 0:
+        return False
+    if x >= WIDTH or y >= HEIGHT:
+        return False
+    return True
+    
--- objects.py
+++ objects.py
@@ -1,9 +1,107 @@
 import pygame 
+import numpy as np
+import env
 
+
+SPEED = 20
 
 class Wall(pygame.sprite.Sprite):
     def __init__(self, x, y, w, h):
         super().__init__()
         self.surf = pygame.Surface((w, h))
-        self.surf.fill((128,255,40))
-        self.rect = self.surf.get_rect(center = (10, 420))
+        self.surf.fill((60,60, 60))
+        self.rect = self.surf.get_rect(center = (x, y))
+
+class Lidar():
+    def __init__(self, surf, x, y, dangle, walls):
+        self.surf = surf
+        self.x = x
+        self.y = y
+        self.angle = -np.pi/4
+        self.dangle = dangle
+        self.walls = walls
+        self.beams = [LaserBeam(self, surf, x, y, self.angle)]
+        self.data = []
+
+    def draw(self):
+        pygame.draw.circle(self.surf, (255, 0, 0), (self.x, self.y), 10)
+        pygame.draw.line(self.surf, (255, 0, 0), (self.x, self.y), (self.x + np.cos(self.angle) * 100, self.y + np.sin(self.angle) * 100))
+
+
+    def update(self):
+        self.draw()
+        self.angle += self.dangle
+        self.beams.append(LaserBeam(self, self.surf, self.x, self.y, self.angle))
+        for beam in self.beams:
+            beam.update()
+        for beam in self.beams:
+            beam.draw()
+        print(len(self.data))
+
+    def beamFeedback(self, beam):
+        for b in self.beams:
+            if beam == b:
+                self.beams.remove(b)
+        ratio = env.FPS * SPEED
+        self.data.append(0)
+        self.data[-1] = [beam.angle, beam.time/2]
+
+class Hitbox():
+    def __init__(self, x, y, w, h):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.time = 0
+
+    def intersects(self, x, y):
+        if x >= self.x and x <= self.x + self.w and y >= self.y and y <= self.y + self.h:
+            return True
+        return False
+    
+
+class LaserBeam():
+    def __init__(self, lidar, surf, x, y, angle) :
+        self.lidar = lidar
+        self.surf = surf
+        self.x = x
+        self.y = y
+        self.angle = angle
+        self.dx = np.cos(self.angle)
+        self.dy = np.sin(self.angle)
+        self.collided = False
+        self.time = 0
+    
+
+    def update(self):
+
+        for i in range(SPEED):
+            if not env.isValid(int(self.x + self.dx), int(self.y + self.dy)):
+                self.collided = True
+                self.dx *= -1
+                self.dy *= -1
+            else:
+                color = self.surf.get_at((int(self.x + self.dx), int(self.y + self.dy)))[:3]
+                if color != (255, 255, 255):
+                    if self.collided and color == (255, 0, 0):
+                        self.lidar.beamFeedback(self)
+                        break
+                    elif not self.collided and color != (255, 0, 0):
+                        self.collided = True
+                        self.dx *= -1
+                        self.dy *= -1
+
+            self.x+=self.dx
+            self.y+=self.dy
+            self.time += 1
+
+    def __eq__(self, other) -> bool:
+        if isinstance(other, LaserBeam):
+            return other.angle == self.angle and other.collided == self.collided
+        return False
+
+    
+    def draw(self):
+        pygame.draw.circle(self.surf, (0, 255, 0), (self.x, self.y), 1)
+
+    
